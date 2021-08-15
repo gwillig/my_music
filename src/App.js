1a774845f5ea8c6d3c17050f49c82a634193b335
gwillig <gustavwillig@gmail.com>
2021-08-15 12:52:44 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,46 +1,164 @@
+import React, { Component } from "react";
+import Modal from "./components/Modal";
+import axios from "axios";
 
-import {useState} from "react";
-import ReactJkMusicPlayer from 'react-jinke-music-player'
-import 'react-jinke-music-player/assets/index.css'
-// import "./styles.css";
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+class App extends Component {
 
-export default function App() {
-  const [audioList,setAudioList] = useState([
-    {
-    name: 'Viva la vida',
-    singer: 'Coldplay',
-    musicSrc:'https://res.cloudinary.com/dvwayyj5d/video/upload/v1617727988/Coldplay_-_Viva_La_Vida_wethoj.mp3'
-  },
-  {
-   name: 'The Scientist',
-   singer: 'Coldplay',
-   musicSrc:'https://res.cloudinary.com/dvwayyj5d/video/upload/v1617727983/Coldplay_-_The_Scientist_jbyuef.mp3'
- }, {
-   name: 'Hymn For The Weekend',
-   singer: 'Coldplay',
-   musicSrc:'https://res.cloudinary.com/dvwayyj5d/video/upload/v1617727974/Coldplay_-_Hymn_For_The_Weekend_Official_Video_tbwvcs.mp3'
- } ,  {
-   name: 'A Sky Full Of Stars',
-   singer: 'Coldplay',
-   musicSrc:'https://res.cloudinary.com/dvwayyj5d/video/upload/v1617727963/Coldplay_-_A_Sky_Full_Of_Stars_szf5aw.mp3'
- }
- ]);
+	constructor(props) {
+		super(props);
+		this.state = {
+			viewCompleted: false,
+			activeItem: {
+				title: "",
+				description: "",
+				completed: false
+			},
+			todoList: []
+		};
+	}
 
- const updateAudioList = ()=>{
-      const newAudioList = [...audioList];
-      newAudioList.splice(2,0,{name:'Paradise',singer:'Coldplay',musicSrc:'https://res.cloudinary.com/dvwayyj5d/video/upload/v1617727909/Coldplay_-_Paradise_Official_Video_px5u9e.mp3'})
-      setAudioList(newAudioList); 
-    }
+	componentDidMount() {
+		this.refreshList();
+	}
 
-  return (
-    <div className="App">
-      <h2>Click on the add song button to add new song in between the 2 and 3 song </h2>
-      <button onClick={updateAudioList}>add song</button>
-      <h3>Songs in audioList</h3> 
-      <ol> 
-        {audioList.map(el=><li>{el.name}</li>)}
-      </ol>
-     <ReactJkMusicPlayer audioLists={audioList}/>
-    </div>
-  );
+	refreshList = () => {
+		axios
+			// .get("http://localhost:8000/api/todos/")
+			// Because of proxy in package.json, command be shorten as follows:
+			.get("/api/todos/")
+			.then(res => this.setState({ todoList: res.data }))
+			.catch(err => console.log(err));
+	};
+
+	displayCompleted = status => {
+		if (status) {
+			return this.setState({ viewCompleted: true });
+		}
+		return this.setState({ viewCompleted: false });
+	};
+
+	renderTabList = () => {
+		return (
+			<div className="my-5 tab-list">
+				<span
+					onClick={() => this.displayCompleted(true)}
+					className={this.state.viewCompleted ? "active" : ""}
+				>
+					Complete
+            </span>
+				<span
+					onClick={() => this.displayCompleted(false)}
+					className={this.state.viewCompleted ? "" : "active"}
+				>
+					Incomplete
+            </span>
+			</div>
+		);
+	};
+	
+	renderItems = () => {
+		const { viewCompleted } = this.state;
+		const newItems = this.state.todoList.filter(
+			item => item.completed === viewCompleted
+		);
+		return newItems.map(item => (
+			<li
+				key={item.id}
+				className="list-group-item d-flex justify-content-between align-items-center"
+			>
+				<span
+					className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""
+						}`}
+					title={item.description}
+				>
+					{item.title}
+				</span>
+				<span>
+					<button
+						onClick={() => this.editItem(item)}
+						className="btn btn-secondary mr-2"
+					>
+						Edit
+					</button>
+					<button
+						onClick={() => this.handleDelete(item)}
+						className="btn btn-danger"
+					>
+						Delete
+					</button>
+				</span>
+			</li>
+		));
+	};
+
+	toggle = () => {
+		this.setState({ modal: !this.state.modal });
+	};
+
+	handleSubmit = item => {
+		this.toggle();
+		if (item.id) {
+			axios
+				// Because of proxy in package.json, command be shorten as follows:
+				// .put(`http://localhost:8000/api/todos/${item.id}/`, item)
+				.put(`/api/todos/${item.id}/`, item)
+				.then(res => this.refreshList());
+			return;
+		}
+		axios
+			// Because of proxy in package.json, command be shorten as follows:
+			// .post("http://localhost:8000/api/todos/", item)
+			.post("/api/todos/", item)
+			.then(res => this.refreshList());
+	};
+
+	handleDelete = item => {
+		axios
+			// Because of proxy in package.json, command be shorten as follows:
+			// .delete(`http://localhost:8000/api/todos/${item.id}`)
+			.delete(`/api/todos/${item.id}/`)
+			.then(res => this.refreshList());
+	};
+
+	createItem = () => {
+		const item = { title: "", description: "", completed: false };
+		this.setState({ activeItem: item, modal: !this.state.modal });
+	};
+
+	editItem = item => {
+		this.setState({ activeItem: item, modal: !this.state.modal });
+	};
+
+	render() {
+		return (
+			<main className="content">
+				<h1 className="text-white text-center my-4">Ray's Todo App</h1>
+				<div className="row ">
+					<div className="col-md-6 col-sm-10 mx-auto p-0">
+						<div className="card p-3">
+							<div className="">
+								<button onClick={this.createItem} className="btn btn-primary">
+									Add task
+                    </button>
+							</div>
+							{this.renderTabList()}
+							<ul className="list-group list-group-flush">
+								{this.renderItems()}
+							</ul>
+						</div>
+					</div>
+				</div>
+				{this.state.modal ? (
+					<Modal
+						activeItem={this.state.activeItem}
+						toggle={this.toggle}
+						onSave={this.handleSubmit}
+					/>
+				) : null}
+			</main>
+		);
+	}
 }
+export default App;
