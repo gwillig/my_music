52d58913bd8eebdb002ebe2d97f7a652ff999cea
gwillig <gustavwillig@gmail.com>
2021-08-15 14:49:34 +0200

    Add icon

--- backend/urls.py
+++ backend/urls.py
@@ -18,12 +18,26 @@ from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 from rest_framework import routers
 from todo import views
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+
 
 router = routers.DefaultRouter()
 router.register(r'todos', views.TodoView, 'todo')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),         
-    path('api/', include(router.urls)),
-    re_path('.*', TemplateView.as_view(template_name='index.html')),
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
+    # Links for pwa
+    path(r'index.html', (TemplateView.as_view(template_name="index.html", content_type='application/html', )),
+        name='index.html'),
+    path('static/', TemplateView.as_view(template_name='index.html')),
+    path(r'sw.js', (TemplateView.as_view(template_name="sw.js", content_type='application/javascript', )),
+        name='sw.js'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
+    path('manifest.json', RedirectView.as_view(url=staticfiles_storage.url('manifest.json'))),
+    path('static/logo192.png', RedirectView.as_view(url=staticfiles_storage.url('img/logo192.png'))),
+    path('static/logo512.png', RedirectView.as_view(url=staticfiles_storage.url('img/logo512.png'))),
 ]
+
