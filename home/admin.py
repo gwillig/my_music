0e30a597ddd03c3373604601ed6d1c2541812379
gwillig <gustavwillig@gmail.com>
2021-08-15 21:31:31 +0200

    Fix| backend| add additional information to admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from home.models import MusicRecord
 
-admin.site.register(MusicRecord)
-# Register your models here.
+class DataAdmin(admin.ModelAdmin):
+    list_display = ['title', "section", 'link']
+
+
+admin.site.register(MusicRecord, DataAdmin)
+
--- src/App.js
+++ src/App.js
@@ -12,79 +12,6 @@ axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
 
-const playListFocus = [
-  {
-    name: 'Focus_1',
-    singer: 'Jay Chou',
-    cover:
-      '',
-    musicSrc:
-      'https://res.cloudinary.com/gwillig/video/upload/v1629028166/music/focus1.mp3',
-  },
-  {
-    name: 'Focus_2',
-    singer: 'Jay Chou',
-    cover:
-      '',
-    musicSrc:
-      'https://res.cloudinary.com/gwillig/video/upload/v1629028145/music/focus2.mp3',
-  },
-  {
-    name: 'Focus_4',
-    singer: 'Jay Chou',
-    cover:
-      '',
-    musicSrc:
-      'https://res.cloudinary.com/gwillig/video/upload/v1629028137/music/focus_4.mp3',
-  },
-]
-
-const playListSleep= [
-  {
-    name: 'Sleep1',
-    singer: 'Jay Chou',
-    cover:
-      '',
-    musicSrc:
-      'https://res.cloudinary.com/gwillig/video/upload/v1629028146/music/sleep1.mp3',
-  },
-  {
-    name: 'Sleep2',
-    singer: 'Jay Chou',
-    cover:
-      '',
-    musicSrc:
-      'https://res.cloudinary.com/gwillig/video/upload/v1629028151/music/sleep2.mp3',
-  },
-  {
-    name: 'Sleep3',
-    singer: 'Jay Chou',
-    cover:
-      '',
-    musicSrc:
-      'https://res.cloudinary.com/gwillig/video/upload/v1629028160/music/sleep3.mp3',
-  },
-  {
-    name: 'Sleep4',
-    singer: 'Jay Chou',
-    cover:
-      '',
-    musicSrc:
-      'https://res.cloudinary.com/gwillig/video/upload/v1629028137/music/focus_4.mp3',
-  },
-]
-
-const playListRelax = [
-  {
-    name: 'Focus_1',
-    singer: 'Jay Chou',
-    cover:
-      '',
-    musicSrc:
-      'https://res.cloudinary.com/gwillig/video/upload/v1629029134/music/relax.mp3',
-  },
-]
-
 
 class App extends React.Component{
 
@@ -93,7 +20,7 @@ class App extends React.Component{
         this.onChangePlayList = this.onChangePlayList.bind(this)
         this.handleFetch = this.handleFetch.bind(this)
         this.state={
-            audioList: playListFocus,
+            audioList: [{}],
             MusicRecord:{}
         }
     }
